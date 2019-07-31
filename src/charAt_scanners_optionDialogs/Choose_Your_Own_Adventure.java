8256c070abfddf79baa3da202696363d53a18a97
League Student <league@iMac-16.attlocal.net>
2019-07-30 19:52:13 -0700

    :+1: :smile: CYOA
    :dragon:

--- src/charAt_scanners_optionDialogs/Choose_Your_Own_Adventure.java
+++ src/charAt_scanners_optionDialogs/Choose_Your_Own_Adventure.java
@@ -13,40 +13,74 @@ public static void main(String[] args) {
 
   if(h == 1) {
 	  JOptionPane.showMessageDialog(null, "Let's prank batman");
+  
+	  int d = JOptionPane.showOptionDialog(null, "Should Robin?", "", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] {"Dump Batmans House?", "Toilet Paper Batmans House?"}, null);
+	  			
+	  if(d == 0) {
+		  JOptionPane.showMessageDialog(null, "One day later Batman walks out of his house and a bunch of dump falls on him and realizes a skunk is in the dump");
+		  
+		  JOptionPane.showMessageDialog(null, "Help Me!!!");
+	  }	 
+	  if(d == 1) {
+		  JOptionPane.showMessageDialog(null, "One day later Batman walks out of his house and a bunch of toilet paper falls on him. he trs to get out and realizes one of the toilet papers are used");
+	  
+		  JOptionPane.showMessageDialog(null, "Help Me!!!");
+	  
+		  
+
+	   
+	  }
+	  
+	  System.exit(0);
+ 
   }
 
+  
+  
   int i = JOptionPane.showConfirmDialog(null, "Is the person gonna answer");
 
   if(i == 0) {
 	  JOptionPane.showMessageDialog(null, "Who is it?");
+  
+	  JOptionPane.showMessageDialog(null, "Robin");
+
+	  JOptionPane.showMessageDialog(null, "Robin who?");
+
+	  JOptionPane.showMessageDialog(null, "Robin your house");
+
+	  JOptionPane.showMessageDialog(null, "Here have the money");
+  System.exit(0);
   }
   
-  JOptionPane.showMessageDialog(null, "Robin");
+  if(i == 1) {
+	  JOptionPane.showMessageDialog(null, "Let's prank batman");
+	  
+	  int d = JOptionPane.showOptionDialog(null, "Should Robin?", "", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] {"Dump Batmans House?", "Toilet Paper Batmans House?"}, null);
+	  			
+	  if(d == 0) {
+		  JOptionPane.showMessageDialog(null, "One day later Batman walks out of his house and a bunch of dump falls on him and realizes a skunk is in the dump");
+		  
+		  JOptionPane.showMessageDialog(null, "Help Me!!!");
+	  }	 
+	  if(d == 1) {
+		  JOptionPane.showMessageDialog(null, "One day later Batman walks out of his house and a bunch of toilet paper falls on him. he trs to get out and realizes one of the toilet papers are used");
+	  
+		  JOptionPane.showMessageDialog(null, "Help Me!!!");
+	  
+		  
 
-  JOptionPane.showMessageDialog(null, "Robin who?");
-
-  JOptionPane.showMessageDialog(null, "Robin your house");
-
+	   
+	  }
+	  
+	  System.exit(0);
  
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
+  }
+  }
+  
 
 
 }
+
+
